61e25f09776366ae340ddd3576cc1aa30a42c378
GonzaloSantizo <san21504@uvg.edu.gt>
2023-11-21 22:37:34 -0600

    added TM + next string function

--- Project3.py
+++ Project3.py
@@ -35,3 +35,46 @@ class Tape(object):
 
   def __setitem__(self, pos, char):
     self.__tape[pos] = char
+
+class TuringMachine(object):
+
+  def __init__(
+    self,
+    tapes = list[str],
+    blankSymbol = " ",
+    initialState = "",
+    finalStates = None,
+    transitionFunction = None,
+    acceptedStates = None
+  ):
+    self.__init = initialState
+    self.tapes = tapes
+    self.__curr = 0
+    self.__blankSymbol = blankSymbol
+    self.__tape = Tape(tapes[self.__curr],self.__blankSymbol)
+    self.__headPosition = 0
+    self.__currentState = initialState
+    self.__immediate_description = None
+    self.__acceptedStates = acceptedStates
+    if transitionFunction == None:
+      self.__transitionFunction = {}
+    else:
+      self.__transitionFunction = transitionFunction
+    if finalStates == None:
+      self.__finalStates = set()
+    else:
+      self.__finalStates = set(finalStates)
+  
+  def nextStringAvailable(self):
+    self.__curr += 1
+    if self.__curr < len(self.tapes):
+      self.__tape = Tape(self.tapes[self.__curr],self.__blankSymbol)
+      self.__headPosition = 0
+      self.__currentState = self.__init
+      self.__immediate_description = None
+      return True
+    else:
+      return False
+
+  def getTape(self): 
+    return str(self.__tape)
